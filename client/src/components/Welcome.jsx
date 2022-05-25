bb8fe6ef95ec8df2c61affa00c9710f1dba8b292
Joel Aduma <caesaragen@gmail.com>
2022-05-25 17:00:29 +0300

    50 minutes into tutorial

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -1,8 +1,59 @@
-import React from 'react'
-
+import React from 'react';
+import { AiFillPlayCircle } from 'react-icons/ai';
+import { SiEthereum } from 'react-icons/si';
+import { BsInfoCircle } from 'react-icons/bs';
+const url = "https://www.youtube.com/watch?v=Wn_Kb3MR_cU";
+import { Loader } from './';
+const commonStyles = "min-h-[70px] sm:px-0 px-2 sm:min-w-[120px] flex justify-center items-center border-[0.5px] border-gray-400 text-sm font-light text-white";
 const Welcome = () => {
+  const connectWallet = () => { };
   return (
-    <div>Welcome</div>
+    <div className="flex w-full items-center justify-center">
+      <div className="flex md:flex-row flex-col items-start justify-between md:p-20-py-12 px-4">
+        <div className='flex flex-1 justify-start flex-col md:mr-10'>
+          <h1 className="text-3xl sm:text-5xl text-white text-gradient py-1">
+            Send Crypto <br /> across the world
+          </h1>
+          <p className='text-left mt-5 text-white font-light md:w-9/12 w-11/12 text-base'>
+            Explore the Crypto world. Buy and sell crypto easily.
+          </p>
+          <button type='button' onClick={connectWallet} className='flex flex-row justify-center items-center my-5 bg-[#2952e3] p-3 rounded-full cursor-pointer hover:bg-[#2546bd]' >
+            <p className='text-white test-base font-semibold'>Connect Wallet</p>
+          </button>
+          <div className='grid sm:grid-cols-3 grid-cols-2 w-full mt-10'>
+            <div className={`rounded-tl-2xl ${commonStyles}`}>
+              Reliability
+            </div>
+            <div className={commonStyles}>
+              Security
+            </div>
+            <div className={`rounded-tr-2xl ${commonStyles}`}>
+              Ethereum
+            </div>
+            <div className={`rounded-bl-2xl ${commonStyles}`}>
+              web 3.0
+            </div>
+            <div className={commonStyles}>
+              Low Fees
+            </div>
+            <div className={`rounded-br-2xl ${commonStyles}`}>
+              Blockchain
+            </div>
+          </div>
+          <div className='flex flex-col flex-1 items-center justify-start w-full md:mt-0 mt-10'>
+            <div className='p-3 rounded-xl h-40 sm:w-72 w-full my-5 eth-card white-glassmorphism flex-col justify-end items-start'>
+              <div className='flex justify-between flex-col w-full h-full'>
+                <div className='flex justify-between items-start'>
+                  <div className='w-10 h-10 rounded-full border-2 border-white flex justify-center items-center'>
+                    <SiEthereum fontSize={21} color="#fff" className='text-white' />
+                  </div>
+                </div>
+              </div>
+              </div>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
